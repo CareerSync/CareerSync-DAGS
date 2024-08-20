3463d41ae07c772208e33e7ba68614b98f9ad40a
CHOI JEE HYUK <akfktl328@kyongi.ac.kr>
2024-08-20 12:52:33 +0900

    jsonl 파일 만드는 부분까지 작성

--- dags/saramin_crawling_dag.py
+++ dags/saramin_crawling_dag.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import json
 import random
 # 기본 설정
@@ -24,19 +23,26 @@ user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
 ]
 
-# Python 함수 정의 (작업 내용)
-# def init_driver(path):
-#     options = webdriver.ChromeOptions()
-#     options.add_argument("--no-sandbox")
-#     options.add_argument("--disable-dev-shm-usage")
-#     return webdriver.Chrome(path, options=options)
+def init_driver(**kwargs):
+    from selenium.webdriver.chrome.options import Options
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('window-size=1200x600')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    # remote_chromedriver 컨테이너에 연결
+    remote_webdriver = 'http://remote_chromedriver:4444/wd/hub'
+    driver = webdriver.Remote(remote_webdriver, options=options)
+    
+    return driver
 
 def get_job_urls(user_agents: list, **kwargs): # driver, page_count
     from bs4 import BeautifulSoup
     import time
     import requests
     url_list = []
-    for page in range(1, 2 + 1): # page_count + 1
+    for page in range(1, 1 + 1): # page_count + 1
         user_agent = user_agents[page % len(user_agents)]
         soup = requests.get(f'https://www.saramin.co.kr/zf_user/search?cat_mcls=2&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&panel_type=&search_optional_item=y&search_done=y&panel_count=y&preview=y&recruitPage={page}&recruitSort=reg_dt&recruitPageCount=200&inner_com_type=&searchword=&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n', headers={'User-Agent': user_agent})
         time.sleep(random.randint(1, 5))
@@ -51,6 +57,7 @@ def get_job_urls(user_agents: list, **kwargs): # driver, page_count
             except Exception:
                 pass
     return url_list
+
 def process_job_urls(**kwargs):
     ti = kwargs['ti']  # Task Instance 가져오기
     url_list = ti.xcom_pull(task_ids='get_job_urls')  # get_job_urls Task에서 XCom으로 저장된 데이터 가져오기
@@ -60,6 +67,160 @@ def process_job_urls(**kwargs):
         print(f"Processing URL: {url}")
         # 여기서 원하는 처리를 수행
 
+def extract_metadata(soup):
+    import re
+    meta = soup.select_one('head > meta:nth-child(6)')
+    if meta:
+        meta_content = meta['content']
+        match = re.search(r'마감일:(\S+)', meta_content)
+        end_date = match.group(1) if match else None
+        return end_date
+    return None
+
+def extract_basic_info(driver):
+    dt = {'경력': 'Work_history', '학력': 'Education', '근무형태': 'Job_type'}
+    job_info = {}
+    for index in range(1, len(dt) + 1):
+        dt_elements = driver.find_elements(By.XPATH, f'//*[@id="content"]/div[3]/section[1]/div[1]/div[2]/div/div[1]/dl[{index}]/dt')
+        dd_elements = driver.find_elements(By.XPATH, f'//*[@id="content"]/div[3]/section[1]/div[1]/div[2]/div/div[1]/dl[{index}]/dd')
+        if dt_elements and dd_elements:
+            dt_text = dt_elements[0].text.strip()
+            if dt_text in dt:
+                key = dt[dt_text]
+                job_info[key] = dd_elements[0].text.strip()
+    return job_info
+
+def extract_additional_info(driver):
+    dt2 = {'급여': 'Salary', '근무지역': 'working area', '근무일시': 'working time', '직급/직책': 'Position', '근무요일': 'working day'}
+    job_info = {}
+    basic_info_element2 = driver.find_elements(By.XPATH, '//*[@id="content"]/div[3]/section[1]/div[1]/div[2]/div/div[2]')
+    for i in basic_info_element2:
+        basic_info2 = i.text
+    parts2 = basic_info2.split('\n')
+    for i in range(0, len(parts2), 2):
+        key = parts2[i]
+        value = parts2[i + 1]
+        if key in dt2:
+            job_info[dt2[key]] = value
+        else:
+            job_info[key] = value
+    return job_info
+
+def extract_job_details(driver, headers):
+    from selenium.webdriver.common.by import By
+    from bs4 import BeautifulSoup
+    import requests
+    import time
+    iframe_html = driver.find_element(By.XPATH, '//*[@id="iframe_content_0"]')
+    iframe_url = iframe_html.get_attribute('src')
+    soup = requests.get(iframe_url, headers=headers)
+    time.sleep(random.randint(1, 2))
+    iframe_soup = BeautifulSoup(soup.text, 'html.parser')
+    
+    for script in iframe_soup.find_all("script"):
+        script.replace_with("")
+    for element in iframe_soup(text=lambda text: isinstance(text, Comment)):
+        element.replace_with("")
+    
+    keyword_list = ['logo', 'icon', 'watermark', 'banner', 'stack', 'topimg', 'interview_pc_1', 'top']
+    img_list = []
+    logo_url = None
+    
+    for img in iframe_soup.find_all("img"):
+        src = img['src']
+        if src.startswith("//"):
+            src = "https:" + src
+            
+        if 'logo' in src:
+            logo_url = src
+        
+        if all(keyword not in src for keyword in keyword_list if keyword != 'logo') and len(src) <= 100:
+            img_list.append(src)
+    
+    driver.switch_to.frame("iframe_content_0")
+    detail_info = driver.find_element(By.CLASS_NAME, "user_content").text
+    
+    return img_list, detail_info, logo_url
+
+def extract_company_name(driver):
+    from selenium.webdriver.common.by import By
+    company_name_element = driver.find_elements(By.XPATH, '//*[@id="content"]/div[3]/section[1]/div[1]/div[1]/div[1]/div[1]/a[1]')
+    for i in company_name_element:
+        return i.text
+    return None
+
+def task2(**kwargs):
+    from selenium.webdriver.common.by import By
+    from bs4 import BeautifulSoup
+    import time
+    import requests
+    driver = init_driver()  # init_driver를 호출하여 driver 객체 생성
+    today = datetime.now().strftime('%Y-%m-%d')
+    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    
+    url_list = kwargs['ti'].xcom_pull(task_ids='get_urls_task')
+    
+    with open('job_data.jsonl', 'w', encoding='utf-8') as f:
+        for index, url in enumerate(url_list):
+            job_info_all = {'Datetime': today}
+            data = requests.get(url, headers=headers)
+            soup = BeautifulSoup(data.text, 'html.parser')
+            driver.get(url)
+            driver.implicitly_wait(time_to_wait=60)
+            start_time_all = time.time()
+            
+            try:
+                job_info_all['title'] = driver.find_element(By.XPATH, '//*[@id="content"]/div[3]/section[1]/div[1]/div[1]/div/h1').text
+                job_info_all['URL'] = url
+            except Exception as e:
+                print(f'Error processing 채용공고 제목 {url}: {e}')
+                pass
+
+            try:
+                end_date = extract_metadata(soup)
+                if end_date:
+                    job_info_all['end_date'] = end_date
+                job_info_all['start_date'] = driver.find_element(By.CLASS_NAME, "info_period").text.split('\n')[1]
+            except Exception as e:
+                print(f'Error processing metadata {url}: {e}')
+                pass
+
+            try:
+                job_info_all.update(extract_basic_info(driver))
+            except Exception as e:
+                print(f'Error processing 기본정보 1 {url}: {e}')
+                pass
+
+            try:
+                job_info_all.update(extract_additional_info(driver))
+            except Exception as e:
+                print(f'Error processing 기본정보 2 {url}: {e}')
+                pass
+
+            try:
+                job_info_all['Co_name'] = extract_company_name(driver)
+            except Exception as e:
+                print(f'Error processing 기업 이름 {url}: {e}')
+                pass
+
+            try:
+                img_list, detail_data, logo_url = extract_job_details(driver, headers)
+                job_info_all['img_list'] = img_list
+                job_info_all['detail_data'] = detail_data
+                job_info_all['logo_url'] = logo_url
+            except Exception as e:
+                print(f'Error processing 채용공고 내용 {url}: {e}')
+                continue
+
+            json.dump(job_info_all, f, ensure_ascii=False)
+            f.write('\n')
+
+            end_time_all = time.time()
+            execution_time_all = end_time_all - start_time_all
+            print(f"{job_info_all.get('Co_name')} 크롤링 소요시간 : {round(execution_time_all, 2)} seconds")
+
+    driver.quit()
+    print('크롤링이 완료되었습니다.')
 
 # DAG 정의
 with DAG(
@@ -70,26 +231,19 @@ with DAG(
     catchup=False,  # 과거 실행을 수행하지 않음
 ) as dag:
 
-    # PythonOperator를 사용한 작업 정의
-    # init_driver_task = PythonOperator(
-    #     task_id='init_driver',
-    #     python_callable=init_driver, # 실행할 Python 함수
-    #     provide_context=True
-    # )
-
-    get_job_urls_task = PythonOperator(
-        task_id='get_job_urls',
-        python_callable=get_job_urls,  # 실행할 Python 함수
-        op_args=[user_agents],  # 함수에 전달할 인자
-        provide_context=True # kwargs를 함수에 전달
-    )
-    process_job_urls_task = PythonOperator(
-        task_id='process_job_urls',
-        python_callable=process_job_urls,  # 실행할 Python 함수
-        provide_context=True  # kwargs를 함수에 전달
+    get_urls_task = PythonOperator(
+        task_id='get_urls_task',
+        python_callable=get_job_urls,
+        op_kwargs=user_agents,
+        dag=dag,
     )
 
-    # DAG에 작업 추가
-    # init_driver_task >> get_job_urls_task
-    get_job_urls_task >> process_job_urls_task
+    crawl_job_task = PythonOperator(
+        task_id='crawl_job_data',
+        python_callable=task2,
+        dag=dag,
+    )
+        # DAG에 작업 추가
+        # init_driver_task >> get_job_urls_task
+    get_urls_task >> crawl_job_task
 
